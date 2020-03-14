2dde422c734cd71104bd059e2566d502d5495621
Corail31 <17954109+Corail31@users.noreply.github.com>
2020-03-14 17:55:13 +0100

    curios bag keybinds not kept on new game
    #3

--- src/main/java/ovh/corail/travel_bag/network/ClientProxy.java
+++ src/main/java/ovh/corail/travel_bag/network/ClientProxy.java
@@ -1,8 +1,13 @@
 package ovh.corail.travel_bag.network;
 
+import com.google.common.reflect.Reflection;
+import ovh.corail.travel_bag.event.ClientEventHandler;
+
 public class ClientProxy implements IProxy {
     @Override
+    @SuppressWarnings("UnstableApiUsage")
     public void preInit() {
+        Reflection.initialize(ClientEventHandler.class);
     }
 
     @Override
